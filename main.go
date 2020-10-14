64c639f9049717e80b49dc8cf8b63ff166e0ec73
kalilcazes <kalilcazes@gmail.com>
2020-10-14 18:46:38 -0300

    refactor: extract code to character.go

--- main.go
+++ main.go
@@ -2,31 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"path/filepath"
 	"regexp"
-	"strings"
 	"time"
 
 	"github.com/gocolly/colly/v2"
+
+	"github.com/KalilCazes/dontstarve-crawler/character"
 )
 
-type character struct {
-	name         string
-	nickname     string
-	motto        string
-	bio          string
-	perk         string
-	profileImage string
-	health       int
-	hunger       int
-	sanity       int
-	favoriteFood map[string]string
-}
-
-func main() {
-
-	var characters []character
+//NewCollector creates a collector with predefined configuration
+func NewCollector() *colly.Collector {
 
 	urlFilters := regexp.MustCompile(`dontstarve.fandom.com/wiki/[^(Special:Log)|
 	^(Template)|^(Help)|^(User)]`)
@@ -40,53 +25,12 @@ func main() {
 		Delay:      1 * time.Second,
 	})
 
-	c.OnHTML("center", func(e *colly.HTMLElement) {
-
-		e.ForEach("tbody", func(_ int, tbody *colly.HTMLElement) {
-			rowIdentifier := 0
-
-			e.ForEach("tr", func(_ int, tr *colly.HTMLElement) {
-				characterIndex := 0
-				tr.ForEach("td", func(_ int, td *colly.HTMLElement) {
-					content := strings.TrimSpace(td.Text)
-
-					switch rowIdentifier % 3 {
-					case 0:
-						var c character
-						c.profileImage = content
-						characters = append(characters, c)
-						// fmt.Printf("[0] characters: %#v\n", characters)
-					case 1:
-						// fmt.Printf("[1] characters: %#v\n", characters)
-						characters[characterIndex].name = content
-					case 2:
-						// fmt.Printf("[2] characters: %#v\n", characters)
-						characters[characterIndex].perk = content
-					}
-					characterIndex++
-
-				})
-				rowIdentifier++
-				characterIndex = 0
-				// write to file for future parsing
-				filePath := fmt.Sprintf("html/%s", filepath.Base(e.Request.URL.Path))
-				err := ioutil.WriteFile(filePath, []byte(e.Text), 0644)
-				if err != nil {
-					fmt.Printf("%#v", err)
-				}
-			})
-		})
-		fmt.Printf("characters: %#v\n", characters)
-	})
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	c.Visit("https://dontstarve.fandom.com/wiki/Characters")
-	fmt.Println("O")
+	return c
+}
+func main() {
+	collector := NewCollector()
+	chars := character.GetCharacters(collector)
+	for _, name := range chars {
+		fmt.Printf("%s: %s\n", name, character.GetInfo(collector, name).Nickname)
+	}
 }
