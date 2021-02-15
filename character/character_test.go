cf6ac34d42fe5d8e8ec172eaaf83589328150ba0
kalilcazes <kalilcazes@gmail.com>
2021-02-15 13:27:26 -0300

    test: add more scenarios

--- character/character_test.go
+++ character/character_test.go
@@ -40,6 +40,33 @@ func TestGetCharacterInfo(t *testing.T) {
 			Sanity:       "200",
 			FavoriteFood: map[string]string{"Wobster Dinner (DST)": "https://dontstarve.fandom.com/wiki/Wobster_Dinner_(DST)"},
 		},
+
+		{
+			Name:         "Wortox",
+			Nickname:     "The Soul Starved",
+			Motto:        "\"Pardon me if I don't shake your hand.\"",
+			Bio:          "Wortox is a mischievous imp whose levity masks a deep well of guilt. He has taken many souls in his time.",
+			Perks:        []string{"Is an imp", "Can hop through time and space", "Less nourished by physical food"},
+			ProfileImage: "https://static.wikia.nocookie.net/dont-starve-game/images/6/6f/Wortox_DST.png",
+			Health:       "200",
+			Hunger:       "175",
+			Sanity:       "150",
+			FavoriteFood: map[string]string{"Pomegranate": "https://dontstarve.fandom.com/wiki/Pomegranate",
+				"Sliced Pomegranate": "https://dontstarve.fandom.com/wiki/Sliced_Pomegranate"},
+		},
+
+		{
+			Name:         "Wilson",
+			Nickname:     "The Gentleman Scientist",
+			Motto:        "\"I will conquer this world with the power of my MIND!\"",
+			Bio:          "At heart Wilson is truly a man of science. It's not in his nature to resist the call of the unknown.",
+			Perks:        []string{"Grows a magnificent beard"},
+			ProfileImage: "https://static.wikia.nocookie.net/dont-starve-game/images/3/33/Wilson_DST.png",
+			Health:       "150",
+			Hunger:       "150",
+			Sanity:       "200",
+			FavoriteFood: map[string]string{"Bacon and Eggs": "https://dontstarve.fandom.com/wiki/Bacon_and_Eggs"},
+		},
 	}
 
 	for _, tt := range tests {
